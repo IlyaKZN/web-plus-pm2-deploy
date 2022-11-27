f625b68140438fc29922fdcf9312b09514578953
IlyaKZN <ilia.pichugin2014@yandex.ru>
2022-11-27 22:28:48 +0300

    1

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -13,13 +13,12 @@ module.exports = {
   // Настройка деплоя
   deploy: {
     production: {
-      key: '~/.ssh/id_ed25519',
       user: DEPLOY_USER,
       host: DEPLOY_HOST,
       ref: DEPLOY_REF,
       repo: 'https://github.com/IlyaKZN/web-plus-pm2-deploy.git',
       path: DEPLOY_PATH,
-      // 'pre-deploy': `scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      'pre-deploy': `scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
       'post-deploy': 'cd ./backend && npm i && npm run start',
     },
   },
