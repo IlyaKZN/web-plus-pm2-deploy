ba2058efe0eb66c8c30124f8056b7306f724f50f
IlyaKZN <ilia.pichugin2014@yandex.ru>
2022-11-27 19:35:26 +0300

    1

---
+++ frontend/ecosystem.config.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+
+const {
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF = 'origin/master',
+} = process.env;
+
+module.exports = {
+  apps: [{
+    name: 'frontend',
+    script: './dist/app.js',
+  }],
+
+  // Настройка деплоя
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: 'https://github.com/IlyaKZN/web-plus-pm2-deploy.git',
+      path: DEPLOY_PATH,
+      'post-deploy': 'npm i && npm run build',
+    },
+  },
+};
