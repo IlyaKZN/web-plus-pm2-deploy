fffbc0efb5b2b3d7a92159521db34163a1f35be9
IlyaKZN <ilia.pichugin2014@yandex.ru>
2022-11-27 20:12:56 +0300

    1

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -18,7 +18,7 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: 'https://github.com/IlyaKZN/web-plus-pm2-deploy.git',
       path: DEPLOY_PATH,
-      'post-deploy': 'npm i && npm run',
+      'post-deploy': 'npm i && ./frontend npm run build',
     },
   },
 };
